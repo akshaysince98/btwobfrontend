0f02fcd46a8ea94512f88df7be13578ea94279f3
Akshay Yadav <akshayhat@gmail.com>
2022-09-26 21:15:55 +0530

    search close done

--- src/components/Searchbox.jsx
+++ src/components/Searchbox.jsx
@@ -1,8 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
+import './searchbox.css'
 
 function Searchbox() {
+  const [seaClo, setSeaClo] = useState('search')
+
+  const searchClose = () => {
+    seaClo == 'search' ? setSeaClo('close') : setSeaClo('search')
+  }
+
+  const searchclosebox = () => {
+    setSeaClo('close')
+  }
+
+  const searching = (e) => {
+    
+  }
+
   return (
-    <div>Searchbox</div>
+    <>
+      <div className='searchboxdiv' >
+        <div onClick={searchClose} className='searchlogo'>{seaClo}</div>
+        <input onClick={searchclosebox} className='searchbox' type="text" placeholder='Search by account name or website' />
+      </div>
+    </>
   )
 }
 
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import './sidebar.css'
 
 function Sidebar() {
   return (
-    <div>Sidebar</div>
+    <>
+      <div className="logotext">
+        <div className="logo">a</div>
+        <div className="text">B2Brain</div>
+      </div>
+    </>
   )
 }
 
 export default Sidebar
+
